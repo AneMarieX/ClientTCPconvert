d91f06deca5be49d88c433d3b001637f8771104c
AneMarieX <33547868+AneMarieX@users.noreply.github.com>
2018-10-11 21:07:16 +0200

    Update Program.cs

--- ClientTCPconvert/Program.cs
+++ ClientTCPconvert/Program.cs
@@ -8,8 +8,34 @@ namespace ClientTCPconvert
 {
     class Program
     {
-        static void Main(string[] args)
+         private static readonly int PORT = 12;
+        static void Main()
         {
+            string localAdress = "127.0.0.1";
+            try
+            {
+                TcpClient clientsocket = new TcpClient(localAdress, PORT);
+                Stream stream = clientsocket.GetStream();
+                StreamWriter swriter = new StreamWriter(stream);
+                StreamReader sreader = new StreamReader(stream);
+                swriter.AutoFlush = true;
+                while (true)
+                {
+                    Console.WriteLine("Possible commands: TOGRAM/TOOUNCES + value");
+                    string msg = Console.ReadLine();
+                    swriter.WriteLine(msg);
+                    Thread.Sleep(1000);
+                    Console.WriteLine(sreader.ReadLine());
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Client was not able to establish connection");
+                Thread.Sleep(4000);
+                Console.WriteLine("Trying to get back...");
+                Main();
+            }
         }
     }
 }
